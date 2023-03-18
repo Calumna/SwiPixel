2f4f79d617d88936655b6f66dd8e2d5b69606065
Jade803 <jade.cothenet@etu.univ-lyon1.fr>
2023-03-18 16:29:50 -0400

    Pick a photo from galery

--- app/src/main/java/com/uqac/swipixel/fragment_home.kt
+++ app/src/main/java/com/uqac/swipixel/fragment_home.kt
@@ -5,8 +5,10 @@ import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.NavigationUI
 
 // TODO: Rename parameter arguments, choose names that match
