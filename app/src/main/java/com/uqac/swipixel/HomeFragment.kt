7a3b27f92bc53b2ef164aeb92069afdda63428f2
Jade803 <jade.cothenet@etu.univ-lyon1.fr>
2023-04-10 22:03:58 -0400

    Correction nb image max + ajout vérification autorisation d'accès multimédia

--- app/src/main/java/com/uqac/swipixel/HomeFragment.kt
+++ app/src/main/java/com/uqac/swipixel/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.uqac.swipixel
 
+import android.Manifest
 import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
@@ -8,7 +9,6 @@ import android.graphics.ImageDecoder
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
-import android.os.ext.SdkExtensions.getExtensionVersion
 import android.provider.MediaStore
 import android.util.Log
 import android.view.LayoutInflater
@@ -25,40 +25,45 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.NavigationUI
-import androidx.recyclerview.widget.RecyclerView
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
 
     private lateinit var cardDeck: Swiper
 
     // variable pour afficher une photo de la galerie
-    var selectedImage: List<SwiperData> = ArrayList<SwiperData>()
+    var selectedImage: List<SwiperData> = ArrayList()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        // setHasOptionsMenu(true)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
 
         val adapter = SwiperAdapter()
-        val pickMultipleMedia = registerForActivityResult(ActivityResultContracts.PickMultipleVisualMedia(1000)) { uris ->
-            // Callback is invoked after the user selects media items or closes the
-            // photo picker.
-            if (uris.isNotEmpty()) {
-                selectedImage = uris.map {
-                    SwiperData(it)
+
+        // vérifier l'autorisation d'accès aux fichiers
+        if(ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.READ_EXTERNAL_STORAGE) !=
+            PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(requireActivity(), arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), 1)
+        } else {
+            val pickMultipleMedia =
+                registerForActivityResult(ActivityResultContracts.PickMultipleVisualMedia(100)) { uris ->
+                    // Callback is invoked after the user selects media items or closes the
+                    // photo picker.
+                    if (uris.isNotEmpty()) {
+                        selectedImage = uris.map {
+                            SwiperData(it)
+                        }
+                        adapter.addData(selectedImage)
+                    }
                 }
-                adapter.addData(selectedImage)
-            }
+            // Ecouter le bouton pour charger l'image
+            val pickButton = root.findViewById<Button>(R.id.button)
+            pickButton.setOnClickListener { pickMultipleMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly)) }
         }
 
-        // Ecouter le bouton pour charger l'image
-        val pickButton = root.findViewById<Button>(R.id.button)
-        pickButton.setOnClickListener { pickMultipleMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly)) }
-
-        cardDeck = root.findViewById<Swiper>(R.id.cardDeck);
+        cardDeck = root.findViewById(R.id.cardDeck);
         cardDeck.adapter = adapter
         return root;
     }
