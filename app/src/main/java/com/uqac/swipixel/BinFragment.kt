e0a1bf2b97393bc50a58840900d80481185db757
Jade803 <jade.cothenet@etu.univ-lyon1.fr>
2023-04-11 23:11:10 -0400

    replace onCreate by onCreateView

--- app/src/main/java/com/uqac/swipixel/BinFragment.kt
+++ app/src/main/java/com/uqac/swipixel/BinFragment.kt
@@ -1,6 +1,9 @@
 package com.uqac.swipixel
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil.setContentView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -13,14 +16,20 @@ class BinFragment : Fragment(R.layout.fragment_bin){
     // Liste d'images dans la corbeille
     var deleteImages: List<SwiperData> = ArrayList<SwiperData>()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
 
-        val deletedImages = getDeletedImages()
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_bin, container, false)
+        deleteImages = getDeletedImages()
+        // Define the RecyclerView
+        val recyclerView: RecyclerView = view.findViewById(R.id.recyclerView)
 
-        bin = RecycleBinAdaptater(deletedImages)
-//        recyclerView.adapter = imageAdapter
-//        recyclerView.layoutManager = LinearLayoutManager(this)
+        // Set the RecyclerView's LayoutManager
+        recyclerView.layoutManager = LinearLayoutManager(activity)
+
+        // Set the RecyclerView's Adapter
+        recyclerView.adapter = RecycleBinAdaptater(deleteImages)
+
+        return view
     }
 
     private fun getDeletedImages(): List<SwiperData> {
